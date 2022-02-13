3f91c1d2bafb460529fed360942e1e3d23a35cf7
Alexey Y <alexey.yashenkov@gmail.com>
2022-02-13 15:59:58 +0300

    Update pairedTag.js

--- pairedTag.js
+++ pairedTag.js
@@ -36,3 +36,17 @@ export default class extends Node {
 //   this.children = children;
 //   this.toString = toString;
 // };
+
+// :: PROTO ::
+// import Node from './node';
+
+// export default function PairedTag(name, attributes, body = '', children = []) {
+//   Node.apply(this, [name, attributes]);
+//   this.body = body;
+//   this.children = children;
+// }
+
+// PairedTag.prototype.toString = function toString() {
+//   const value = this.children.length > 0 ? this.children.join('') : this.body;
+//   return `<${this.name}${this.getAttributesAsLine()}>${value}</${this.name}>`;
+// };
