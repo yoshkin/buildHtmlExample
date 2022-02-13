1940beee838ccf29fcf62877b3b1e88a0139e43b
Alexey Y <alexey.yashenkov@gmail.com>
2022-02-13 15:25:09 +0300

    Update pairedTag.js

--- pairedTag.js
+++ pairedTag.js
@@ -17,3 +17,22 @@ export default class extends Node {
     return `<${this.name}${this.getAttributesAsLine()}>${value}</${this.name}>`;
   }
 }
+
+
+// :: FUNC ::
+// import Node from './node';
+
+// function toString() {
+//   const value = this.children.length > 0
+//     ? this.children.join('')
+//     : this.body;
+
+//   return `<${this.getName()}${this.getAttributesAsLine()}>${value}</${this.getName()}>`;
+// }
+
+// export default function PairedTag(name, attributes = {}, body = '', children = []) {
+//   Node.apply(this, [name, attributes]);
+//   this.body = body;
+//   this.children = children;
+//   this.toString = toString;
+// };
