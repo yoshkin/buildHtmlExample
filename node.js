4d428d28a8ee1f99861dc71f561ab81681cb0226
Alexey Y <alexey.yashenkov@gmail.com>
2022-02-13 15:24:34 +0300

    Update node.js

--- node.js
+++ node.js
@@ -9,3 +9,21 @@ export default class {
       .reduce((acc, key) => `${acc} ${key}="${this.attributes[key]}"`, '');
   }
 }
+
+
+// :: FUNC ::
+// function getName() {
+//   return this.name;
+// }
+
+// function getAttributesAsLine() {
+//   return Object.keys(this.attributes)
+//     .reduce((acc, key) => `${acc} ${key}="${this.attributes[key]}"`, '');
+// }
+
+// export default function Node(name, attributes) {
+//   this.name = name;
+//   this.attributes = attributes;
+//   this.getName = getName.bind(this);
+//   this.getAttributesAsLine = getAttributesAsLine.bind(this);
+// };
