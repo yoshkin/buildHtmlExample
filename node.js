8118f2c9052da8aae32de16edb40fe5b8ca3818f
Alexey Y <alexey.yashenkov@gmail.com>
2022-02-13 12:10:20 +0300

    Create node.js

---
+++ node.js
@@ -0,0 +1,11 @@
+export default class {
+  constructor(name, attributes = {}) {
+    this.name = name;
+    this.attributes = attributes;
+  }
+
+  getAttributesAsLine() {
+    return Object.keys(this.attributes)
+      .reduce((acc, key) => `${acc} ${key}="${this.attributes[key]}"`, '');
+  }
+}
