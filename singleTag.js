ff2de60c5e982f18fce93b42cc390831ce0b37c2
Alexey Y <alexey.yashenkov@gmail.com>
2022-02-13 12:09:09 +0300

    Update singleTag.js

--- singleTag.js
+++ singleTag.js
@@ -1,18 +1,8 @@
 // @ts-check
+import Node from './Node';
 
-export default class {
-  constructor(name, attributes = {}) {
-    this.name = name;
-    this.attributes = attributes;
-  }
-
+export default class extends Node {
   toString() {
-    return `<${this.name}${this.getAttributesAsLine()}>`
+    return `<${this.name}${this.getAttributesAsLine()}>`;
   }
-
-  getAttributesAsLine() {
-    return Object.entries(this.attributes)
-      .map(([key, value]) => ` ${key}="${value}"`)
-      .join('');
-  }
-};
+}
