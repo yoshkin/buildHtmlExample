8d34ed7fda15c155d20a600148f7f7462fd8a177
Alexey Y <alexey.yashenkov@gmail.com>
2022-02-13 16:14:27 +0300

    Update node.js

--- node.js
+++ node.js
@@ -27,3 +27,22 @@ export default class {
 //   this.getName = getName;
 //   this.getAttributesAsLine = getAttributesAsLine;
 // };
+
+
+// :: PROTO ::
+// function getName() {
+//   return this.name;
+// }
+
+// function getAttributesAsLine() {
+//   return Object.keys(this.attributes)
+//     .reduce((acc, key) => `${acc} ${key}="${this.attributes[key]}"`, '');
+// }
+
+// export default function Node(name, attributes) {
+//   this.name = name;
+//   this.attributes = attributes;
+// };
+
+// Node.prototype.getName = getName;
+// Node.prototype.getAttributesAsLine = getAttributesAsLine;
